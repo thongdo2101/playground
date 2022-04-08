2a2a8fe0b6f6905867ee28a6a58fab4d678820d8
Thong Do <thongdv@godgroup.co>
2022-04-08 09:06:22 +0700

    feat: update

--- create-folder-godrive/main.go
+++ create-folder-godrive/main.go
@@ -7,23 +7,23 @@ import (
 	"github.com/go-resty/resty/v2"
 )
 
-const path = "Teams/Team Xuân Ninh/2022"
-const nameFolder = "Nhung"
+const path = "Teams/2022/GFT/FBA"
+const nameFolder = "Hường"
 
 // const monthFolder = "August"
 // const monthFolder = "September"
-const monthFolder = "January"
+const monthFolder = "April"
 
 // const monthFolder = "November"
 // const monthFolder = "December"
 
 // const monthNumber = "08"
 // const monthNumber = "09"
-const monthNumber = "01"
+const monthNumber = "04"
 
 // const monthNumber = "11"
 // const monthNumber = "12"
-const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDE0Mjk3NzUsInVzZXJJRCI6ImMxNWYzNmQyLTU5NGItNDQwNC1hMTJjLTUxYmQ4NzM0MGQ2ZiIsImVtYWlsIjoidGhvbmdkdkBnb2Rncm91cC5jbyIsInJvbGUiOiJTVVBFUl9BRE1JTiIsInpvbmUiOiIifQ.j4_bnIMTkPKPDGpdWQnawJHumjA6uJDUn15j8xrQ3oE"
+const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDkzOTE3MTMsInVzZXJJRCI6ImMxNWYzNmQyLTU5NGItNDQwNC1hMTJjLTUxYmQ4NzM0MGQ2ZiIsImVtYWlsIjoidGhvbmdkdkBnb2Rncm91cC5jbyIsInJvbGUiOiJTVVBFUl9BRE1JTiIsInpvbmUiOiIifQ.BoailK128LexCyi_7-Lb2HLoZZKHUD__YDaBo6JKDE0"
 
 func createNameFolder() {
 	client := resty.New()
@@ -54,14 +54,14 @@ func createMonthFolder() {
 }
 
 func createDaysFolder() {
-	for i := 1; i < 32; i++ {
+	for i := 1; i < 31; i++ {
 		time.Sleep(500)
 		client := resty.New()
 		url := "https://contents.godmerch.com/api/v1/files/create-folder"
 		temp := fmt.Sprintf("%s/%s/%s/%s", path, nameFolder, monthFolder, fmt.Sprintf("%s.%v", monthNumber, i))
-    if i < 10 {
-      temp = fmt.Sprintf("%s/%s/%s/%s", path, nameFolder, monthFolder, fmt.Sprintf("%s.0%v", monthNumber, i))
-    }
+		if i < 10 {
+			temp = fmt.Sprintf("%s/%s/%s/%s", path, nameFolder, monthFolder, fmt.Sprintf("%s.0%v", monthNumber, i))
+		}
 		res, err := client.R().SetHeader("Authorization", token).SetBody(map[string]interface{}{
 			"path": temp,
 		}).Post(url)
@@ -74,7 +74,7 @@ func createDaysFolder() {
 }
 
 func main() {
-	createNameFolder()
+	// createNameFolder()
 	createMonthFolder()
 	createDaysFolder()
 	fmt.Println("done")
